ec5cd0ad4c62ccaddd61693a563bb819c8f46774
nic2703 <niclas_scheuer@web.de>
2024-05-16 17:48:23 +0200

    geofind

--- Nic/geoident/geofindurban.py
+++ Nic/geoident/geofindurban.py
@@ -1,17 +1,18 @@
 import random
 import numpy as np
 from shapely.geometry import Polygon, Point
+from typing import Optional
 import geopandas as gpd
 import os
 from shapely.ops import unary_union
 from shapely.affinity import translate
 
-country_json_file='shapefiles\\urban_shp\\ne_50m_urban_areas.shp'
+urban_json_file='shapefiles\\urban_shp\\ne_50m_urban_areas.shp'
 
-def generate_random_point_in_urban_area():
+def generate_random_point_in_urban_area(n: Optional[int] = None):
     # Load the GeoJSON file and create a spatial index
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    shp_file_path = os.path.join(script_dir, country_json_file)
+    shp_file_path = os.path.join(script_dir, urban_json_file)
     urban_areas = gpd.read_file(shp_file_path)
     sindex = urban_areas.sindex
 
@@ -38,8 +39,16 @@ def generate_random_point_in_urban_area():
             if poly.contains(random_point):
                 return random_point
 
-    urban_polygon = select_random_polygon(urban_areas)
-    random_point = generate_random_point_in_polygon(urban_polygon)
-    return random_point.y, random_point.x
+    if n is None:
+        urban_polygon = select_random_polygon(urban_areas)
+        random_point = generate_random_point_in_polygon(urban_polygon)
+        return random_point.y, random_point.x
+    else:
+        coordinates = []
+        for _ in range(n):
+            urban_polygon = select_random_polygon(urban_areas)
+            random_point = generate_random_point_in_polygon(urban_polygon)
+            coordinates.append((random_point.y, random_point.x))
+        return coordinates
 
-print(generate_random_point_in_urban_area())
+print(generate_random_point_in_urban_area(5))
