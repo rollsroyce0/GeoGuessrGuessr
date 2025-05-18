074324a287845c04a679bf3b874dbc8ec638b96c
rollsroyce0 <werderr@ethz.ch>
2025-05-19 00:45:46 +0200

    new best models

--- Roy/ML/Geoguessrmodel_Trainer_silent.py
+++ Roy/ML/Geoguessrmodel_Trainer_silent.py
@@ -258,7 +258,7 @@ scheduler = ReduceLROnPlateau(
 #######################################
 batch_size_data = 232
 train_loader = DataLoader(list(zip(X_train, y_train)), batch_size=batch_size_data, shuffle=True)
-epochs = 402
+epochs = 502
 losses = []
 val_losses = []
 min_val_loss = 1e5
